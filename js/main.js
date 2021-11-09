f136826c06a6509493e07369ab0ed271b6cf0bd1
danamerro <danamerro@gmail.com>
2021-11-09 20:41:11 -0300

    se modificaron cosas en JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var resultado = 0;
+
 function estudiantePromo(){
     document.getElementById("pago").innerHTML ="";
     var resultado= 200-(200*0.8);
@@ -17,5 +19,32 @@ function juniorPromo(){
 }
 
 function botonBorrar(){
-    document.getElementById("pago").innerHTML="";
+    resultado = 0;
+    document.getElementById("pago").innerHTML = "";
+}
+
+function botonDescuento(){
+    var seleccion = document.getElementById("listaCategoria")
+    var cantidad = document.getElementById("cantidadEntradas")
+
+    if (seleccion.value == 1){
+        console.log("test")
+
+        document.getElementById("pago").innerHTML = "";
+        resultado += (200 - (200 * 0.8)) * cantidad.value;
+        document.getElementById("pago").innerHTML += resultado;
+    
+    }else if (seleccion.value == 2){
+
+        document.getElementById("pago").innerHTML = "";
+        resultado += (200 - (200 * 0.5)) * cantidad.value;
+        document.getElementById("pago").innerHTML += resultado;
+
+    }else if (seleccion.value == 3){
+
+        document.getElementById("pago").innerHTML = "";
+        resultado += (200 - (200 * 0.15)) * cantidad.value;
+        document.getElementById("pago").innerHTML += resultado;
+
+    }
 }
